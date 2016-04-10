f8eb41222767db85e52045f75f50955f679375d4
Bruno Sanches <bruno_sanches15@hotmail.com>
2016-04-10 20:54:10 -0300

    -Finalização das configurações JPA e Hibernate

--- mvc/src/main/java/br/com/estudo/model/Meta.java
+++ mvc/src/main/java/br/com/estudo/model/Meta.java
@@ -1,11 +1,28 @@
 package br.com.estudo.model;
 
-public class Meta {
-	
-	private int minutes;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
+public class Meta {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+	private int minutes;
+		
+	public Long getId() {
+		return id;
+	}
+	
 	public int getMinutes() {
 		return minutes;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
 	}
 
 	public void setMinutes(int minutes) {
